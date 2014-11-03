a3cae98f7b1f9a99b38455dc5f5f36e7a68991c1
youcune <you@nakanishi.email>
2014-11-03 18:49:59 +0900

    updated Google Analytics script

--- source/javascripts/google-analytics.js
+++ source/javascripts/google-analytics.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-42388188-5', 'auto');
+ga('create', 'UA-42388188-6', 'auto');
 ga('send', 'pageview');
